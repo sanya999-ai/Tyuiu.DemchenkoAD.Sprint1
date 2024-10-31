f6b7a60526d0dda645bcb3a3df8d75d5cc3b68ba
Александра Демченко <Tyuiu.DemchenkoAD@outlook.com>
2024-10-31 11:57:11 +0500

    Task7

--- Tyuiu.DemchenkoAD.Sprint1.Task7.V15.Lib/DataService.cs
+++ Tyuiu.DemchenkoAD.Sprint1.Task7.V15.Lib/DataService.cs
@@ -5,8 +5,8 @@ namespace Tyuiu.DemchenkoAD.Sprint1.Task7.V15.Lib
     {
         public double Calculate(double x)
         {
-            double res = Math.Pow(3, 2) - Math.Pow(3, 3) - Math.Cos(Math.Pow(2, 3))
-                        + (7 * Math.Pow(2, 2) / Math.Pow(2, 3)) - (15 * 2);
+            double res = Math.Pow(3, 2) - Math.Pow(3, 3) - Math.Cos(Math.Pow(2, 3)) + (7 * Math.Pow(2, 2) / Math.Pow(2, 3)) - (15 * 2);
+          
             return res;
 
         }
