340f638f393fd1fe1da5d77a5b5f8faddc014ec6
Александра Демченко <Tyuiu.DemchenkoAD@outlook.com>
2024-10-31 12:25:59 +0500

    Task6

--- Tyuiu.DemchenkoAD.Sprint1.Task6.V6.Lib/DataService.cs
+++ Tyuiu.DemchenkoAD.Sprint1.Task6.V6.Lib/DataService.cs
@@ -9,12 +9,12 @@ namespace Tyuiu.DemchenkoAD.Sprint1.Task6.V6.Lib
                 string[] arrOfStr = value.Split(' ');
 
 
-                string res = " ";
+                string res = "";
 
 
                 foreach (string a in arrOfStr)
                 {
-                    res += a.Substring(1, a.Length - 1) + " ";
+                    res += a.Substring(1, a.Length - 1) + "";
                 }
 
                 return res;
